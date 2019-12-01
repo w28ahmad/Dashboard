6720682ab7a760391ed35e297af9ecd5c9a1e6f3
wahab ahmad <w28ahmad@uwaterloo.ca>
2019-12-01 15:03:43 -0500

    Adding a way to upload Image in cnn section

--- app/cnnDashboard/callbacks.py
+++ app/cnnDashboard/callbacks.py
@@ -1,13 +1,40 @@
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
 import plotly
 import plotly.graph_objs as go
 
-import pandas as pd
+import datetime
 
-from webapp.app.config import BASEDIR
+# from webapp.app.config import BASEDIR
 
 # Colors
 from cnnDashboard.style import app_colors
 
 def register_callbacks(dashapp):
-    pass
+    def parse_contents(contents, filename, date):
+        return html.Div([
+        html.P(filename),
+        html.P(datetime.datetime.fromtimestamp(date)),
+
+        # HTML images accept base64 encoded strings in the same format
+        # that is supplied by the upload
+        html.Img(src=contents, style={"width": "100%"}),
+    ], 
+    style={
+        'backgroundColor': app_colors['background'],
+        'height':'1000px',
+        'margin-left': "2%",
+        # 'width': '87%',
+        'float':'right'
+})
+    
+    @dashapp.callback(Output('output-image-upload', 'children'),
+              [Input('upload-image', 'contents')],
+              [State('upload-image', 'filename'),
+               State('upload-image', 'last_modified')])
+    def update_output(list_of_contents, list_of_names, list_of_dates):
+        if list_of_contents is not None:
+            children = [
+                parse_contents(list_of_contents, list_of_names, list_of_dates)
+                ]
+            return children
--- app/cnnDashboard/layout.py
+++ app/cnnDashboard/layout.py
@@ -7,8 +7,40 @@ import pandas as pd
 from cnnDashboard.style import app_colors
 
 layout = html.Div(children=[
-    html.Div(children=[
+    html.Div(children=[# Title
         html.H1(children='Object Detection', 
-                style={'margin': '1%', 'font-weight': 'bold', 'color':app_colors['text']}),
-    ], style={'backgroundColor': app_colors['background'], 'height':'1200px','width': '87%', 'float':'right'}),
-])    
+                style={
+                    'margin': '1%',
+                    'font-weight': 'bold', 
+                    'color':app_colors['text']
+                    }),
+    ]),
+    html.Div(children=[# Upload Image section
+        dcc.Upload(
+            id='upload-image',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files')
+            ]),
+            style={
+                'width': '99%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px',
+            },
+            # Allow multiple files to be uploaded
+            multiple=False
+        ),
+        html.Div(id='output-image-upload'),
+    ], style={}),
+],
+    style={
+        'backgroundColor': app_colors['background'],
+        'height':'1200px',
+        'width': '87%',
+        'float':'right'
+        })    
